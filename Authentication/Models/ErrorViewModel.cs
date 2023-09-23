0734dc27cd0ec572c20e15a86b7753976a7b7c54
aamiraminali <aamir.amin@logicielservice.com>
2023-09-23 22:50:33 +0200

    Authentication through Identity

---
+++ Authentication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Authentication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
